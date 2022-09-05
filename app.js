fec2ce58a938acf12ca8ac82e05ad004350e00b4
Aswanth Alakkadan <aswanth682@gamil.com>
2022-09-05 16:59:30 +0530

    login,logout

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 var fileUpload=require('express-fileupload')
 var db=require('./config/connection')
 process.env.PWD=process.cwd()
+var session=require('express-session')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.use(express.static(process.env.PWD+'/public/product-images'))
+app.use(session({secret:"Key",cookie:{maxAge:600000}}))
 db.connect((err)=>{
   if(err)
   console.log("connection error"+err);
